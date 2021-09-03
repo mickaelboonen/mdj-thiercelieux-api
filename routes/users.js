323d352abc89ddd1e7e9a1dfd582185866ec3d41
Lestrarcher <mickaelboonen@gmail.com>
2021-09-03 18:21:30 +0200

    login ok

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,29 @@ const jwt = require('jsonwebtoken');
 // Destructuring
 var { dbData } = require('../database/db');
 
-router.all('/login', auth.jwtAuthProtected);
+// router.all('/login', auth.jwtAuthProtected);
+
 function generateAccessToken(username) {
   return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+function authenticateToken(req, res, next) {
+  console.log(123456);
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
 }
 
 /* GET users listing. */
@@ -40,17 +60,19 @@ router.post('/check_auth', (req, res, next) => {
   db.end();
 });
 /* GET users listing. */
-// router.post('/login', (req, res, next) => {
-//   const db = mysql.createConnection(dbData);
-//   db.connect();
-//   const { body: {username, password} } = req;
-//   db.query(`SELECT * FROM users WHERE name = '${username}' OR email = '${username}'`, (error, results, fields) => {
-//     if (error) throw error;
-//     console.log(results);
-//     res.send(results)
-//   });
-//   db.end();
-// });
+router.post('/login', authenticateToken, (req, res, next) => {
+  const db = mysql.createConnection(dbData);
+  db.connect();
+  const { body: {id} } = req;
+  db.query(`SELECT * FROM users WHERE id=${id}`, (error, results, fields) => {
+
+    // TODO : 
+    // Inner Join and co 
+    if (error) throw error;
+    res.send(results)
+  });
+  db.end();
+});
 
 
 
